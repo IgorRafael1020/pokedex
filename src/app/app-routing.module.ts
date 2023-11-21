a00151de7fba424eeecffda525bb0a260f66a2aa
Igor <igorrafael_9@hotmail.com>
2023-11-21 16:43:31 -0300

    Ajuste rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  //{ path: '', redirectTo: 'pokedex', pathMatch: 'full' },
+  { path: '', redirectTo: 'pokedex', pathMatch: 'full' },
   { path: 'pokedex', component: HomeComponent },
   { path: '**', redirectTo: 'pokedex' }
 ];
